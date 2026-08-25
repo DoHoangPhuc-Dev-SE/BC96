ec0fe39e09dcbea0e5bff4c996e30ea4a209784a
DoHoangPhuc-Dev-SE <pdo53550@gmail.com>
2026-08-25 23:29:41 +0700

    upd  buoi 16

--- Buoi/Buoi16/js/buoi16.js
+++ Buoi/Buoi16/js/buoi16.js
@@ -45,3 +45,83 @@ console.log("✅ tinhTong_v4", tinhTong_v4(2,3));
 // có thể bỏ () ở phần tham số
 let tinhBinhPhuong = so => so * so
 console.log("✅ tinhBinhPhuong", tinhBinhPhuong(4));
+
+// kiemTraSoChan nhận vào 1 số => kiem tra chẵn lẻ
+
+let kiemTraSoChan = (so) => {
+//    if (so % 2 == 0) {
+//        return true;
+//    } else return false;
+    return so % 2 == 0;
+};
+// tối ưu code
+// let kiemTraSoChan_v2 = (so) => so % 2 == 0;
+
+let kiemTraSoChan_v2 = so => so % 2 == 0 ? "Đây là số chẵn":"Đây là số lẻ";
+console.log("✅ kiemTraSoChan_v2", kiemTraSoChan_v2(8));
+
+let KTtuoi = (tuoi) => {
+    if(tuoi >= 18) console.log("Đủ tuổi")
+    else console.log("Ko Đủ tuổi")
+}
+
+let KTtuoi_v2 = tuoi => tuoi >= 18 ? "Đủ tuổi" : "Ko đủ tuổi"
+
+// Default parameter (tham số mặc định)
+// xinChao có giá trị dự phòng khi người dùng không truyền dữ liệu
+// => hạn chế bị undefined
+
+let xinChao = (name = "bạn") => {
+    console.log("✅ Xin chào", name);
+}
+
+xinChao("Sumoi")
+xinChao() // quên truyền
+
+// ham tinh giam gia  (giaTien = 0 , phanTramGiam = 0)
+let tinhGiamGia = (giaTien = 0, phanTramGiam = 0) => {
+    let tienGiam = giaTien * phanTramGiam / 100
+    let thanhTien = giaTien - tienGiam
+    return thanhTien
+}
+
+console.log("✅ giam 20%", tinhGiamGia(100000, 20).toLocaleString());
+console.log("✅ khong giam", tinhGiamGia(100000).toLocaleString());
+console.log("✅ giam", tinhGiamGia(100000, 10).toLocaleString());
+
+console.log("✅ khong co gi", tinhGiamGia().toLocaleString());
+// Output nếu có default giaTien = 0: "✅ khong co gi 0"
+// Output nếu KHÔNG có default giaTien: "✅ khong co gi NaN"
+
+// callback function : 1 hàm được truyền vào hàm khác dưới dạng tham số
+// hàm được gọi lại
+// callback_fn : sẽ là 1 hàm nào đó bất kỳ
+let thucHienChucNang = (callback_fn) => {
+    console.log("✅ ==== Vào hàm callback ======");
+    callback_fn();
+    // () đại diện cho việc gọi hàm
+    console.log("✅ ==== Kết thúc hàm callback ======");
+};
+
+// 
+thucHienChucNang(xinChao);
+
+// callback có dữ liệu
+let hienThiKetQua = ketQua =>{
+    console.log("✅ ketQua", ketQua);
+}
+
+let hienThiKetQuaDep = ketQua =>{
+    console.log("======= Kết quả =======");
+    console.log(ketQua);
+    console.log("======= END =======");
+}
+
+let tinhTong_cb = (a, b, fnc) =>{
+    let tong = a + b
+    fnc(tong)
+}
+
+tinhTong_cb(3, 5 , hienThiKetQua)
+// tong = 8 => hienThiKetQua(8) =>
+tinhTong_cb(30, 5 , hienThiKetQuaDep)
