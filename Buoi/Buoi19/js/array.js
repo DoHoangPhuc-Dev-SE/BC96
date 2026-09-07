638a0d7b42cbfc84c5900b82d6d1bccfe1ecdfea
DoHoangPhuc-Dev-SE <pdo53550@gmail.com>
2026-09-08 00:05:58 +0700

    upd  array

--- Buoi/Buoi19/js/array.js
+++ Buoi/Buoi19/js/array.js
@@ -1,3 +1,7 @@
 let height_sv1 = 150
 // array : 1 biến chứa nhiều giá trị
 // variable : biến chứa 1 giá trị
+//
+let arrHeight = [150, 165, 145, 170]
+console.log("✅ arr", arrHeight);
+console.log("✅ label", height_sv1);
