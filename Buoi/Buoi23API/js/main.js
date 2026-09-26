c32a433e8be452f0d27446ee13b3d07555a9e49f
DoHoangPhuc-Dev-SE <pdo53550@gmail.com>
2026-09-26 23:00:02 +0700

    finish  buoi 23

--- Buoi/Buoi23API/js/main.js
+++ Buoi/Buoi23API/js/main.js
@@ -1,5 +1,15 @@
 // import : nhập
-import { tinhToan } from "./tinhToan";
+import { tinhToan } from "./tinhToan.js";
 
 const ketQua = tinhToan(3,5)
 document.getElementById("ketQua").innerText = ketQua
+
+function click() {
+  alert("hello");
+}
+
+// đăng ký hàm với window
+window.clickHandle = click; // giúp window lấy biến cục bộ
+// ko được trùng tên 
+// ko được gọi là click nên phải đặt biến khác - clickHandle
+// muốn dùng button 1 là window 2 là dom trên JS
